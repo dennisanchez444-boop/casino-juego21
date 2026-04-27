e9922c5d3def7d8ae26fd9c76a2e81af8791a5e4
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-04-26 21:07:05 -0500

    Prueba Inicial Lista

---
+++ src/com/krakedev/juegos/test/TestConstructorDealer.java
@@ -0,0 +1,12 @@
+package com.krakedev.juegos.test;
+
+import com.krakedev.juegos.servicios.Dealer;
+
+public class TestConstructorDealer {
+
+	public static void main(String[] args) {
+		Dealer d = new Dealer(); 
+		d.imprimirNaipe();
+	}
+
+}
